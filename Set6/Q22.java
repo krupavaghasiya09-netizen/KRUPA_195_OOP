fa6546171e69292616b4e7f62a3c1c1de0421f2c
Krupa_195 <256949786+krupavaghasiya09-netizen@users.noreply.github.com>
2026-04-10 22:33:13 +0530

    Q22_Add Library and BookNotAvailableException classes

---
+++ Set6/Q22.java
@@ -0,0 +1,38 @@
+class BookNotAvailableException extends Exception {
+    public BookNotAvailableException(String message) {
+        super(message);
+    }
+}
+
+class Library {
+    int availableBooks;
+
+       Library(int books) {
+        availableBooks = books;
+    }
+
+    void issueBook(int count) throws BookNotAvailableException {
+        if (count <= availableBooks) {
+            availableBooks = availableBooks - count;
+            System.out.println("Book issued successfully");
+        } else {
+            throw new BookNotAvailableException("Requested books not available");
+        }
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Library lib = new Library(3); 
+
+        try {
+            lib.issueBook(2); 
+            lib.issueBook(2); 
+        }
+        catch (BookNotAvailableException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+    }
+}
